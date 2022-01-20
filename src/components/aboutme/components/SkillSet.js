962c26ab0aa4e02ae72410ed97a16201388294fc
Steven-Rodriguez-git <steven_980213@hotmail.com>
2022-01-20 00:44:29 -0500

    Start page about

---
+++ src/components/aboutme/components/SkillSet.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./SkillSet.css";
+import { Typography } from "@mui/material";
+import SkillBox from "./SkillBox";
+import Grid from "@mui/material/Grid";
+
+const SkillSet = () => {
+  return (
+    <React.Fragment>
+      <Grid
+        className="skillBack"
+        container
+        direction="column"
+        justify="space-around"
+        alignItems="center"
+        spacing={3}
+      >
+        <Grid item xs>
+          <Typography variant="h3" style={{ color: "#fafafa" }}>
+            Skill Sets
+          </Typography>
+        </Grid>
+        <Grid item xs>
+          <Grid
+            className="skillBack"
+            container
+            direction="row"
+            justify="space-around"
+            alignItems="center"
+            spacing={3}
+          >
+            <Grid item xs>
+              <SkillBox />
+            </Grid>
+            <Grid item xs>
+              <SkillBox />
+            </Grid>
+            <Grid item xs>
+              <SkillBox />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+};
+
+export default SkillSet;
