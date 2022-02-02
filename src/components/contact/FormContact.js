ee9ce9a6bc791878c37549314db63aebd510640d
Steven-Rodriguez-git <steven_980213@hotmail.com>
2022-02-01 22:30:03 -0500

    complete form

--- src/components/contact/FormContact.js
+++ src/components/contact/FormContact.js
@@ -1,45 +1,63 @@
 import React from "react";
 import { Grid, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import "../../styles/Contact.css";
+
 const FormContact = () => {
   return (
-    <Grid container spacing={2} >
-      <Grid container
+    <Grid container spacing={2}>
+      <Grid
+        container
         direction="column"
         justifyContent="center"
         alignItems="center"
-        textAling="center"
+        textAlign="center"
         item
         xs={12}
         md={6}
-        style={{ backgroundColor: "#64a3ff", height: "700px"}}
+        style={{ backgroundColor: "#64a3ff", height: "700px" }}
       >
-        <Typography variant="h3" style={{ color: "#fafafa" }}>
-          Contact me!
-        </Typography>
-        <img
-          src="./img/creative.svg"
-          alt=" Developer Vector http://www.freepik.com Designed by stories "
-          style={{ height: "250px", margin:"25px"}}
-        />
+        <Grid item>
+          <Link
+            to="contact"
+            style={{ color: "inherit", textDecoration: "inherit" }}
+          >
+            <Typography variant="h3" style={{ color: "#fafafa" }}>
+              Contact me!
+            </Typography>
+            <img
+              className="imageContact"
+              src="./img/creative.svg"
+              alt=" Developer Vector http://www.freepik.com Designed by stories "
+            />
+          </Link>
+        </Grid>
       </Grid>
       <Grid
-        container 
+        container
+        item
         xs={12}
         md={6}
         direction="column"
         justifyContent="center"
         alignItems="center"
-        textAling="center"
+        textAlign="center"
         style={{ backgroundColor: "#004bb8", height: "700px" }}
       >
-        <Typography variant="h3" style={{ color: "#fafafa" }}>
-          Loking to hire?
-        </Typography>
-        <img
-          src="./img/developer.svg"
-          alt="Developer Create Vector http://www.freepik.com Designed by stories "
-          style={{ height: "250px", margin:"30px" }}
-        />
+        <Link
+          to="contact"
+          style={{ color: "inherit", textDecoration: "inherit" }}
+        >
+          <Typography variant="h3" style={{ color: "#fafafa" }}>
+            Loking to hire?
+          </Typography>
+          <img
+            className="imageContact"
+            src="./img/developer.svg"
+            alt="Developer Create Vector http://www.freepik.com Designed by stories "
+          />
+        </Link>
       </Grid>
     </Grid>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import CenterHead from "../components/home/CenterHead";
 import CenterText from "../components/home/CenterText";
+import AboutMe from "./AboutMe";
+import MyWork from "./MyWork";
+import Contact from "./Contact";
+import Skills from "./Skills"
 
 const Home = () => {
     return (
     <div id="home"> 
-        <CenterHead/>
-        <CenterText/>
+      <CenterHead/>
+      <CenterText/>
+      <AboutMe/>
+      <Skills/>
+      <MyWork />
+      <Contact />
       </div>
     );
   };
