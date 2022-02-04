12c12cb7368e1f0b7d91d15e055df65d4ee2b6a0
Steven-Rodriguez-git <steven_980213@hotmail.com>
2022-02-04 16:50:04 -0500

    Updates

---
+++ precache-manifest.4a4c6fd812cbef53a5296501b05c1354.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dac77e515ea119dcce15f12e458755f",
+    "url": "/portfolio-web-dev/index.html"
+  },
+  {
+    "revision": "f3479185862fe8495f19",
+    "url": "/portfolio-web-dev/static/css/main.6da42c08.chunk.css"
+  },
+  {
+    "revision": "3abad880d9872cad4ac4",
+    "url": "/portfolio-web-dev/static/js/2.e2177c4c.chunk.js"
+  },
+  {
+    "revision": "063f0fae93867ede5ba0c43cc901ca17",
+    "url": "/portfolio-web-dev/static/js/2.e2177c4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d372505667bda3cd8e9",
+    "url": "/portfolio-web-dev/static/js/3.388e1c30.chunk.js"
+  },
+  {
+    "revision": "f3479185862fe8495f19",
+    "url": "/portfolio-web-dev/static/js/main.24149915.chunk.js"
+  },
+  {
+    "revision": "8c614b279062d2da1d41",
+    "url": "/portfolio-web-dev/static/js/runtime-main.7f7c2f34.js"
+  }
+]);
