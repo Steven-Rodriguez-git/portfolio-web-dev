21eb66922ba0773d9e5e17f63d918207d9544abe
Steven-Rodriguez-git <steven_980213@hotmail.com>
2022-04-09 12:06:07 -0500

    Updates

--- precache-manifest.09878c91fff000bf12d8342ec4a5bb52.js
+++ precache-manifest.09878c91fff000bf12d8342ec4a5bb52.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "625ad4fd124f0ccf84b810b18f2b576a",
+    "revision": "68bf9135896b26e54fe570084e95115e",
     "url": "/portfolio-web-dev/index.html"
   },
   {
-    "revision": "b4708e4038c8fc5c854d",
+    "revision": "2bf58ab7af49d494387f",
     "url": "/portfolio-web-dev/static/css/main.40d2d84e.chunk.css"
   },
   {
-    "revision": "fa6d2edfdd9321962036",
-    "url": "/portfolio-web-dev/static/js/2.ca763fa3.chunk.js"
+    "revision": "72ab2dae0fccfd83acf1",
+    "url": "/portfolio-web-dev/static/js/2.a65d5973.chunk.js"
   },
   {
     "revision": "063f0fae93867ede5ba0c43cc901ca17",
-    "url": "/portfolio-web-dev/static/js/2.ca763fa3.chunk.js.LICENSE.txt"
+    "url": "/portfolio-web-dev/static/js/2.a65d5973.chunk.js.LICENSE.txt"
   },
   {
     "revision": "8a982c25600720cfab71",
     "url": "/portfolio-web-dev/static/js/3.97159f69.chunk.js"
   },
   {
-    "revision": "b4708e4038c8fc5c854d",
-    "url": "/portfolio-web-dev/static/js/main.bdc3ccaa.chunk.js"
+    "revision": "2bf58ab7af49d494387f",
+    "url": "/portfolio-web-dev/static/js/main.6a35b923.chunk.js"
   },
   {
     "revision": "ea5c1cdfb1238b9a49b5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-web-dev/precache-manifest.24b3cbdb87a0c6929519fa8ce661610a.js"
+  "/portfolio-web-dev/precache-manifest.09878c91fff000bf12d8342ec4a5bb52.js"
 );
 
 self.addEventListener('message', (event) => {
